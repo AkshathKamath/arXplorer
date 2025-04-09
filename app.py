b755231dc855b1d7203eea7c22645921606399bc
SanjanaB123 <thisissanjana78@gmail.com>
2025-04-09 19:11:57 -0400

    changes

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ async def summarize_paper(request: Request):
         raise HTTPException(status_code=500, detail=str(e))
       
 if __name__ == '__main__':
-	uvicorn.run(app, host='0.0.0.0', port=4000)
+	uvicorn.run(app, host='0.0.0.0', port=4200)
